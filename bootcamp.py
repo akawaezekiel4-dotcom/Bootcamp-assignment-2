0029849fc3f11a5b1afe9bcda6c76aaeb42d460e
akawaezekiel4-dotcom <akawaezekiel4@gmail.com>
2026-06-13 19:50:34 +0100

    Add files via upload

---
+++ bootcamp.py
@@ -0,0 +1,57 @@
+message = 'Hello, World!'
+print(message)
+
+message = 'Goodbye'
+print(message)
+
+name = 'Ezekiel'
+message = (f' Hi {name}')
+print(message)
+
+greeting = 'Good' 'Morning!'
+print(greeting)
+
+greeting = 'Good'
+time = ' Afternoon'
+greeting = greeting + time + "!"
+print(greeting)
+
+str = 'python string'
+
+print(str[0])  # p
+print(str[1])  # y
+print(str[2])  # t
+print(str[3])  # h
+print(str[4])  # o
+print(str[5])  # n
+print(str[6])
+print(str[7])  # s
+print(str[8])  # t
+print(str[9])  # r
+print(str[10]) # i
+print(str[11]) # n
+print(str[12]) # g
+
+str = 'python string'
+str_len = len(str)
+print(str_len)
+
+str = 'python string'
+print(str[0:13])
+
+str = 'python string'
+new_str = 'J' + str[1:]
+print(new_str)
+name = 'Ezekiel'
+message = f'Hello, {name}!'
+print (message )
+x= 5
+y= 4
+total = x + y 
+print (total)
+subtraction = x - y
+print(subtraction)
+multiplication = x * y 
+print (multiplication)
+division = x/y
+print (division )
